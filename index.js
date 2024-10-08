d8737ade4ecdc6b89b619842d07a8d05dbae6611
lorna gathumbi <nyawiralornah@gmail.com>
2024-10-08 12:10:13 +0300

    js event listening

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
-}
+    const button = document.getElementById("button");
+    console.log(button);
+    if(button) {
+        button.addEventListener("click", () => {
+            alert("Btton was clicked!");
+        })
+    }else{
+        console.error("Button not found!");
+    }    
+} 
+
+
